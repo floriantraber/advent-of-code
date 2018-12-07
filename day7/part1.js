8c35eab0c7bef4b8c1c11319d8dda9ca0a5c9cfc
Florian Traber <github@floriantraber.de>
2018-12-07 19:34:07 +0100

    completed day 7.
    Signed-off-by: Florian Traber <github@floriantraber.de>

--- day7/part1.js
+++ day7/part1.js
@@ -2,8 +2,6 @@ const os = require('os');
 const fs = require('fs');
 
 const calculate = function (input) {
-    const abc = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase();
-    const noOfWorkers = 5;
     const stepInstructionsMap = new Map();
     input.forEach(x => {
         const mustFinish = x.substring(5, 6);
@@ -19,32 +17,18 @@ const calculate = function (input) {
     });
     let stepInstructions = [...stepInstructionsMap.values()];
     stepInstructions.sort((a, b) => a.step.localeCompare(b.step));
-    let second = 0;
-    let workInProgressList = [];
+    const steps = [];
     do {
-        if (workInProgressList.length > 0) {
-            workInProgressList.forEach(x => x.tick--);
+        const candidates = stepInstructions.filter(x => x.stepsThatMustFinishBefore.length === 0);
+        if (candidates.length === 0) {
+            throw new Error('No candidate found.');
         }
-        const doneWorks = workInProgressList.filter(x => x.tick === 0);
-        doneWorks.forEach(doneWork =>
-            stepInstructions.forEach(x =>
-                x.stepsThatMustFinishBefore = x.stepsThatMustFinishBefore.filter(y => y !== doneWork.step)));
-
-        workInProgressList = workInProgressList.filter(x => x.tick > 0);
-
-        while (workInProgressList.length < noOfWorkers) {
-            const candidates = stepInstructions.filter(x => x.stepsThatMustFinishBefore.length === 0);
-            if (candidates.length === 0) {
-                break;
-            }
-            const step = candidates[0].step;
-            const tick = 60 + abc.indexOf(step) + 1;
-            stepInstructions = stepInstructions.filter(x => x.step !== candidates[0].step);
-            workInProgressList.push({step, tick});
-        }
-        second++;
-    } while (workInProgressList.length > 0);
-    return --second;
+        steps.push(candidates[0].step);
+        stepInstructions = stepInstructions.filter(x => x.step !== candidates[0].step);
+        stepInstructions.forEach(x =>
+            x.stepsThatMustFinishBefore = x.stepsThatMustFinishBefore.filter(y => y !== candidates[0].step));
+    } while (stepInstructions.length > 0);
+    return steps.reduce((a, b) => a + b);
 };
 
 
@@ -65,7 +49,7 @@ function test(input, ex) {
 }
 
 console.time('A');
-/*test(
+test(
     [
         'Step C must be finished before step A can begin.',
         'Step C must be finished before step F can begin.',
@@ -74,6 +58,6 @@ console.time('A');
         'Step B must be finished before step E can begin.',
         'Step D must be finished before step E can begin.',
         'Step F must be finished before step E can begin.'
-    ], 15);*/
+    ], 'CABDFE');
 main();
 console.timeEnd('A');
